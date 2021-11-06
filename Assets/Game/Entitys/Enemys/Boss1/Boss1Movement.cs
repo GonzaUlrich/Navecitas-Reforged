fc3f5e765bf3d406e6cd0d87deb898a552cab061
GonzaUlrich <GonzaUlrich@gmail.com>
2021-11-06 17:31:41 -0300

    Boss1 Finalizado

--- Assets/Game/Entitys/Enemys/Boss1/Boss1Movement.cs
+++ Assets/Game/Entitys/Enemys/Boss1/Boss1Movement.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class Boss1Movement : MonoBehaviour
 {
     [SerializeField]
-    private GameObject cam; 
+    private GameObject pos; 
+    [Range(0.0f,10f)]
+    [SerializeField]
+    private float movementSpeed =3;
 
-    void Update()
-    {
-            //Vector3 pos = Camera.main.ScreenToWorldPoint(Screen.width,Screen.height);
+    private void Start() {
+        pos = GameObject.Find("GoToBoss1");
+    }
+
+    void Update(){
+        Vector3 dir = pos.transform.position - transform.position;
+        float angle = Mathf.Atan2(dir.y,dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.position += transform.right * Time.deltaTime * movementSpeed;
     }
 }
